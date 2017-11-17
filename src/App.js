3e9e1c6634d828341b66bef0120e4b71bfeafe57
Jonas <jonas.costa@icloud.com>
2017-11-17 08:52:09 -0200

    Using Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state = {
@@ -25,6 +26,7 @@ class App extends Component {
     };
 
     person.name = event.target.value;
+
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
@@ -53,20 +55,22 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangeHandler(event, person.id)}
-            />
+            return <ErrorBoundary key={person.id}>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            </ErrorBoundary>
           })}
         </div>
       );
 
       btnClass = classes.Red;
-      
-    
+
+
     }
 
     //let classes = ['red', 'bold'].join(' '); //red bold
@@ -87,7 +91,7 @@ class App extends Component {
         <p className={assignedClasses.join(' ')}>This is really working!!</p>
 
         <button
-        className={btnClass}
+          className={btnClass}
           onClick={this.togglePersonHandler}>
           Toggle Person
         </button>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,12 @@ import classes from './Person.css';
 
 
 const person = (props) => {
+    const rdn = Math.random();
+    
+            if(rdn > 0.7){
+                throw new Error('Something went wrong!');
+            }
+
     return(
         <div className = {classes.Person}>
                 <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
