ada5b407855d568fbec33ad123564fa29062d9a3
Jonas <jonas.costa@icloud.com>
2017-11-20 08:47:39 -0200

    A Different aproach to HOCs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
-import WithClass from '../../../hoc/WithClass';
+import withClass from '../../../hoc/withClass';
+import Aux from '../../../hoc/Aux';
 
-class Person extends Component{
+class Person extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         console.log('[Person.js] Inside Constructor', props);
     }
-      componentWillMount(){
+    componentWillMount() {
         console.log('[Person.js] Inside componentWillMount()');
-      }
-    
-      componentDidMount(){
-        console.log('[Person.js] Inside componentDidMount()');
-      }
+    }
 
-    render(){
+    componentDidMount() {
+        console.log('[Person.js] Inside componentDidMount()');
+    }
+
+    render() {
         console.log('[Persons.js] Inside render()');
         return (
-            <WithClass classes={classes.Person}>
+
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </WithClass >
+            </Aux>
         )
     }
 }
@@ -31,4 +33,4 @@ class Person extends Component{
 
 
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Aux';
 
 
 class App extends PureComponent {
@@ -102,7 +103,8 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
+
+      <Aux>
         <button onClick={() => { this.setState({ showPersons: true }) }}>show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -111,10 +113,11 @@ class App extends PureComponent {
           clicked={this.togglePersonHandler} />
 
         {persons}
-      </WithClass>
+      </Aux>
+
 
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
